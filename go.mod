1b847f1fc711aedf9dc309eebcc4f935ae160712
James Grant <james@queeg.org>
2020-04-27 10:17:02 +0100

    Initial version

---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/Noddy76/w1temp2mqtt
+
+go 1.14
+
+require (
+	github.com/eclipse/paho.mqtt.golang v1.2.0
+	golang.org/x/net v0.0.0-20200324143707-d3edc9973b7e // indirect
+)
---
+++ w1temp2mqtt.go
@@ -0,0 +1,72 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"strconv"
+	"strings"
+	"time"
+
+	mqtt "github.com/eclipse/paho.mqtt.golang"
+)
+
+func main() {
+	var deviceID = flag.String("device", "", "1wire device ID to report on")
+	var interval = flag.Duration("interval", 10*time.Second, "How often to read the device")
+
+	var mqttBroker = flag.String("broker", "tcp://localhost:1883", "MQTT Broker connext string")
+	var mqttClientID = flag.String("clientid", "w1temp2mqtt", "MQTT client ID")
+	var mqttTopic = flag.String("topic", "w1temp2mqtt", "MQTT topic to publish lines on")
+
+	flag.Parse()
+
+	if len(*deviceID) == 0 {
+		log.Fatal("device must be specified")
+	}
+
+	if strings.Index(*deviceID, "28-") != 0 {
+		log.Fatalf("Device must be a family 28 device (DS18B20 or similar)")
+	}
+
+	opts := mqtt.NewClientOptions().
+		AddBroker(*mqttBroker).
+		SetClientID(*mqttClientID)
+	mqttClient := mqtt.NewClient(opts)
+	if token := mqttClient.Connect(); token.Wait() && token.Error() != nil {
+		log.Fatal(token.Error())
+	}
+	defer mqttClient.Disconnect(250)
+
+	ticker := time.NewTicker(*interval)
+	defer ticker.Stop()
+	for range ticker.C {
+		data, err := ioutil.ReadFile("/sys/bus/w1/devices/" + *deviceID + "/w1_slave")
+		if err != nil {
+			log.Printf("Problem reading value from device (%v)", err)
+			continue
+		}
+
+		raw := string(data)
+
+		var i = strings.Index(raw, "t=")
+		if i == -1 {
+			log.Println("Unable to extract temperature from device")
+			continue
+		}
+		raw = strings.Trim(raw[i+2:], "\t\r\n ")
+
+		temperature, err := strconv.ParseFloat(raw, 64)
+		if err != nil {
+			log.Printf("Unable to parse temperature value (%v)", err)
+			continue
+		}
+		temperature = temperature / 1000.0
+
+		text := fmt.Sprintf("{\"temperature\":%0.3f}", temperature)
+		log.Printf("%s : %s", *mqttTopic, text)
+		token := mqttClient.Publish(*mqttTopic, 0, false, text)
+		token.Wait()
+	}
+}
